962ac1db4fb3787de8dbaf0e68218eef41153b27
Jaimie Zhu <jaimiezhu@jaimiezhu-mbp.local>
2015-06-18 16:49:43 -0700

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Jaimie Zhu on 6/18/15.
+//  Copyright (c) 2015 Jaimie Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Jaimie Zhu on 6/18/15.
+//  Copyright (c) 2015 Jaimie Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
